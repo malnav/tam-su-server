00ec5160a7a99a5c7053b2969e882f700dba4faa
malnav <lamhv89@gmail.com>
2021-09-02 21:38:02 +0700

    my second commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const URI = 'mongodb+srv://malnav:malnav@malnav.dg4da.mongodb.net/mernMemories?r
 
 app.use(express.json({limit: '30mb'}))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-//app.use(cors())
+app.use(cors())
 
 //routes
 app.use('/api/v1/memories',memoriesRoutes);
